8674d46801162533a5ee9abd355ff2bbf61c0ec0
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-10 17:52:49 -0600

    created spec

---
+++ balanced_parenthesis_test.rb
@@ -0,0 +1,44 @@
+require 'json'
+require 'rspec/autorun'
+require_relative 'balanced_parenthesis.rb'
+
+
+describe "BalancedParenthesis" do
+  context "base cases" do
+
+# good examples
+    it "()" do
+      expect(BalancedParenthesis.is_balanced?("()")).to be_truthy
+    end
+
+    it "" do
+      expect(BalancedParenthesis.is_balanced?("")).to be_truthy
+    end
+
+    it "(())" do
+      expect(BalancedParenthesis.is_balanced?("(())")).to be_truthy
+    end
+
+    it "((()()()()))" do
+      expect(BalancedParenthesis.is_balanced?("((()()()()))")).to be_truthy
+    end
+
+#bad examples
+    it "())" do
+      expect(BalancedParenthesis.is_balanced?("())")).to be_falsey
+    end
+
+    it "())" do
+      expect(BalancedParenthesis.is_balanced?("())")).to be_falsey
+    end
+
+    it "((((" do
+      expect(BalancedParenthesis.is_balanced?("((((")).to be_falsey
+    end
+
+    it "(((()))" do
+      expect(BalancedParenthesis.is_balanced?("(((()))")).to be_falsey
+    end
+
+  end
+end

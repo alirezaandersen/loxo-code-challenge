bea5b8f455bd9a0b8855b3b4d1aebee613f0e667
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-10 17:54:30 -0600

    7 passing 1 failure

--- balanced_parenthesis.rb
+++ balanced_parenthesis.rb
@@ -4,4 +4,21 @@ require 'pry'
 
 class BalancedParenthesis
 
+  def self.is_balanced?(string)
+    return false if string.length.odd?
+
+    pairs = {'(' => ')' }
+
+    string.chars.each_with_object([]) do |paren, arr|
+      if pairs.keys.include?(paren)
+        arr << paren
+      elsif pairs.values.include?(paren)
+        return false unless pairs[arr.pop] == paren
+      else
+        return false
+      end
+    end
+    true
+  end
+
 end

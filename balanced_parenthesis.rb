cc09435d660af5b11563da7ca9135ad4b41c60d9
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-10 17:55:39 -0600

    all passing specs

--- balanced_parenthesis.rb
+++ balanced_parenthesis.rb
@@ -3,22 +3,39 @@ require 'rspec/autorun'
 require 'pry'
 
 class BalancedParenthesis
-
   def self.is_balanced?(string)
     return false if string.length.odd?
 
-    pairs = {'(' => ')' }
+    pairs = { '(' => ')' }
 
-    string.chars.each_with_object([]) do |paren, arr|
-      if pairs.keys.include?(paren)
-        arr << paren
-      elsif pairs.values.include?(paren)
-        return false unless pairs[arr.pop] == paren
+    arr = []
+    string.chars do |paren|
+      if expected_pairs = pairs[paren]
+        arr << expected_pairs
       else
-        return false
+        return false unless arr.pop == paren
       end
     end
-    true
+    arr.empty?
   end
+
+  # first attempt fails for matching string for spec 35. I realize that return true was the error
+  # instead I should return see if there is an empty array.
+  # def self.is_balanced?(string)
+  #   return false if string.length.odd?
+  #
+  #   pairs = {'(' => ')' }
+  #
+  #   string.chars.each_with_object([]) do |paren, arr|
+  #     if pairs.keys.include?(paren)
+  #       arr << paren
+  #     elsif pairs.values.include?(paren)
+  #       return false unless pairs[arr.pop] == paren
+  #     else
+  #       return false
+  #     end
+  #   end
+  #   true
+  # end
 
 end

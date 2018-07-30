9fc9798d201432c990bb3255bd12900d63b0711a
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-30 09:46:32 -0600

    corrected syntax

--- balanced_parenthesis.rb
+++ balanced_parenthesis.rb
@@ -3,6 +3,7 @@ require 'rspec/autorun'
 require 'pry'
 
 class BalancedParenthesis
+
   def self.is_balanced?(string)
     return false if string.length.odd?
 
@@ -18,24 +19,5 @@ class BalancedParenthesis
     end
     arr.empty?
   end
-
-  # first attempt fails for matching string for spec 35. I realize that return true was the error
-  # instead I should return see if there is an empty array.
-  # def self.is_balanced?(string)
-  #   return false if string.length.odd?
-  #
-  #   pairs = {'(' => ')' }
-  #
-  #   string.chars.each_with_object([]) do |paren, arr|
-  #     if pairs.keys.include?(paren)
-  #       arr << paren
-  #     elsif pairs.values.include?(paren)
-  #       return false unless pairs[arr.pop] == paren
-  #     else
-  #       return false
-  #     end
-  #   end
-  #   true
-  # end
 
 end
